7a994a900b5c26da0fa13fcb42c8666a65fc2951
Matheus Rubim <matheus.rubim0408@gmail.com>
2025-01-21 10:17:07 -0300

    Project created

---
+++ src/test/java/com/projetointensivo/dslist/DslistApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projetointensivo.dslist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DslistApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
